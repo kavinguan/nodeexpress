5f0e2689eccb29a0c35b14a0cf260f4ebee83dd7
guankaifei <guankaifei@qq.com>
2020-01-17 16:58:05 +0800

    删除mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,68 @@
 var express = require('express');
 var router = express.Router();
 const ejs = require('ejs')
-var {
-  mongoose,
-  User
-} = require("../common/mongoose")
+// var {
+//   mongoose,
+//   User
+// } = require("../common/mongoose")
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    list: '123'
+  });
+});
 
 /* GET home page. */
-User.find({}, function (err, docs) {
-  if (err) {
-    console.log('err:', err);
-    res.json(docs)
-    return;
-  }
-  console.log(docs)
-  // res.json(docs)
-  router.get('/', function (req, res, next) {
-    res.render('index', {
-      title: 'Express',
-      list: docs
-    });
-  });
-})
+// User.find({}, function (err, docs) {
+//   if (err) {
+//     console.log('err:', err);
+//     res.json(docs)
+//     return;
+//   }
+//   console.log(docs)
+//   // res.json(docs)
+//   router.get('/', function (req, res, next) {
+//     res.render('index', {
+//       title: 'Express',
+//       list: docs
+//     });
+//   });
+// })
 
 
-router.get("/getuser", function (req, res) {
-  User.find({}, function (err, docs) {
-    if (err) {
-      console.log('err:', err);
-      res.json(docs)
-      return;
-    }
-    console.log(docs, 'index')
-    res.json(docs)
-    ejs.renderFile('./views/index.ejs', {
-      title: '123',
-      list: docs
-    }, (err, str) => {
-      if (err) {
-        console.log(err)
-        return
-      }
-      res.send(str)
-    })
-  })
-})
+// router.get("/getuser", function (req, res) {
+//   User.find({}, function (err, docs) {
+//     if (err) {
+//       console.log('err:', err);
+//       res.json(docs)
+//       return;
+//     }
+//     console.log(docs, 'index')
+//     res.json(docs)
+//     ejs.renderFile('./views/index.ejs', {
+//       title: '123',
+//       list: docs
+//     }, (err, str) => {
+//       if (err) {
+//         console.log(err)
+//         return
+//       }
+//       res.send(str)
+//     })
+//   })
+// })
 
-router.get("/addUser", function (req, res) {
-  let data = {
-    name: req.query.myinput
-  }
-  console.log(data)
-  var addUser = new User(data)
-  addUser.save()
-  // res.send(JSON.stringify(data))
-  data.success = true
-  res.json(data)
-})
+// router.get("/addUser", function (req, res) {
+//   let data = {
+//     name: req.query.myinput
+//   }
+//   console.log(data)
+//   var addUser = new User(data)
+//   addUser.save()
+//   // res.send(JSON.stringify(data))
+//   data.success = true
+//   res.json(data)
+// })
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var {
-  mongoose,
-  User
-} = require("../common/mongoose")
+// var {
+//   mongoose,
+//   User
+// } = require("../common/mongoose")
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
-User.find({
-  name: '1111'
-}, function (err, docs) {
-  if (err) {
-    console.log('err:', err);
-    res.json(docs)
-    return;
-  }
-  console.log(docs, 111111)
-  // res.json(docs)
-  router.get('/', function (req, res, next) {
-    res.render('login', {
-      title: '用户登录',
-      list: docs
-    });
-  });
-})
+// User.find({
+//   name: '1111'
+// }, function (err, docs) {
+//   if (err) {
+//     console.log('err:', err);
+//     res.json(docs)
+//     return;
+//   }
+//   console.log(docs, 111111)
+//   // res.json(docs)
+//   router.get('/', function (req, res, next) {
+//     res.render('login', {
+//       title: '用户登录',
+//       list: docs
+//     });
+//   });
+// })
 
 module.exports = router;
